cc3f57ff03b3900340ddb9da1a7aa0bf362a8ee0
Ethan Zimbelman <ethan.zimbelman@slack-corp.com>
2022-08-17 09:47:54 -0700

    added the "create new GitHub issue" workflow

---
+++ trigger.ts
@@ -0,0 +1,18 @@
+import { Trigger } from "deno-slack-api/types.ts";
+
+const trigger: Trigger = {
+  type: "shortcut",
+  name: "Create GitHub issue",
+  description: "Create a new GitHub issue in a repository",
+  workflow: "#/workflows/create_issue_workflow",
+  inputs: {
+    interactivity: {
+      value: "{{data.interactivity}}",
+    },
+    channel: {
+      value: "{{data.channel_id}}",
+    },
+  },
+};
+
+export default trigger;
